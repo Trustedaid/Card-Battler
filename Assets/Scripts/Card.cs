9eb54bdddbaa06964123779bde18486ea3ee1db2
Trustedaid <92570103+Trustedaid@users.noreply.github.com>
2023-02-08 14:27:19 +0300

    Holding Hands
    *Setting Up Our Hand
    *Setting Card Position In Hand
    *Preventing Overlapping Cards
    *Moving Cards Smoothly
    *Raising Cards While Hovering

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,14 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.Mathematics;
 
 
 public class Card : MonoBehaviour
 {
     public CardScriptableObject cardSO;
-    
+
     public int currentHealth;
     public int attackPower;
     public int manaCost;
@@ -16,18 +18,29 @@ public class Card : MonoBehaviour
     public TMP_Text healthText, attackText, costText, nameText, actionDescriptionText, loreText;
 
     public Image characterArt, bgArt;
-    
+
+    private Vector3 targetPoint;
+    private Quaternion targetRot;
+
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 540f;
+
+    public bool inHand;
+    public int handPosition;
+
+    private HandController theHC;
 
 
     // Start is called before the first frame update
     void Start()
     {
-       SetUpCard();
+        SetUpCard();
+
+        theHC = FindObjectOfType<HandController>();
     }
 
     public void SetUpCard()
     {
-        
         currentHealth = cardSO.currentHealth;
         attackPower = cardSO.attackPower;
         manaCost = cardSO.manaCost;
@@ -42,13 +55,34 @@ public class Card : MonoBehaviour
 
         characterArt.sprite = cardSO.characterSprite;
         bgArt.sprite = cardSO.bgSprite;
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position,targetPoint,moveSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotateSpeed * Time.deltaTime);
+    }
+
+    public void MoveToPoint(Vector3 pointToMoveTo, Quaternion rotToMatch)
+    {
+        targetPoint = pointToMoveTo;
+        targetRot = rotToMatch;
+    }
+
+    private void OnMouseOver()
+    {
+        if (inHand)
+        {
+            MoveToPoint(theHC.cardPositions[handPosition] + new Vector3(0f, 1f, 0.5f), quaternion.identity);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (inHand)
+        {
+            MoveToPoint(theHC.cardPositions[handPosition], theHC.minPos.rotation);
+        }
     }
 }
